7b8d077e164c97b2e0b35772830d2071197c2cfb
aminekun90 <mohamed.amine.bouzahar@gmail.com>
2026-01-27 15:48:02 +0100

    Json format for today api

--- backend/src/api/v1/health_api.py
+++ backend/src/api/v1/health_api.py
@@ -1,20 +1,39 @@
+import json
 import os
-from datetime import date
+from datetime import date, datetime
+
 from fastapi import APIRouter, Response
+
 from src.core.repository_factory import RepositoryContainer
 
 router = APIRouter()
 repos = RepositoryContainer()
 
+
 @router.get("/health")
 def health():
     return Response(content="OK", status_code=200)
+
+
 @router.get("/today")
 def today():
-    return Response(content=date.today().strftime("%Y-%m-%d %H:%M:%S"), status_code=200)
+    return Response(
+        content=json.dumps(
+            {
+                "date": date.today().strftime("%Y-%m-%d"),
+                "datetime": datetime.today().strftime("%Y-%m-%d %H:%M:%S"),
+                "version": "0.1.0",
+                "status": "OK",
+            }
+        ),
+        status_code=200,
+    )
+
+
 @router.get("/version")
 def version():
     return Response(content="0.1.0", status_code=200)
+
 
 @router.get("/logs")
 def get_logs():
@@ -30,6 +49,7 @@ def get_logs():
         content = f.read()
 
     return Response(content=content, media_type="text/plain")
+
 
 @router.get("/db_health")
 def db_health():
