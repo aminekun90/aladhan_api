0fb87d9e817a9bee0d6d92fd55edad2e8f16eb73
aminekun90 <mohamed.amine.bouzahar@gmail.com>
2026-02-23 15:05:30 +0100

    fixup: fix csv generation

--- frontend/src/components/prayersDataGrid.tsx
+++ frontend/src/components/prayersDataGrid.tsx
@@ -1,16 +1,14 @@
 import { PrayerColumn, PrayerRow } from '@/models/prayer';
-import { DataGrid, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-grid';
-function CustomToolbar() {
-    return (
-        <GridToolbarContainer>
-            <GridToolbarExport />
-        </GridToolbarContainer>
-    );
-}
+import { DataGrid } from '@mui/x-data-grid';
+// function CustomToolbar() {
+//     return (
+//         // <GridToolbarContainer>
+//         //     <GridToolbarExport />
+//         // </GridToolbarContainer>
+//     );
+// }
 export default function PrayersDataGrid({ data, loading }: Readonly<{ data: { rows: PrayerRow[], columns: PrayerColumn[] }, loading: boolean }>) {
     return (
-        <DataGrid {...data} loading={loading} slots={{
-            toolbar: CustomToolbar,
-        }} />
+        <DataGrid {...data} loading={loading} showToolbar />
     )
 }
